0d100a9e661c61f40cf813821455017f63b259a9
Pick1a1username <xscreensaver@outlook.com>
2022-04-24 15:59:49 +0900

    db configured.

--- chapter3/mern_skeleton/server/server.js
+++ chapter3/mern_skeleton/server/server.js
@@ -1,5 +1,16 @@
-import config from './../config/config'
 import app from './express'
+import mongoose from 'mongoose'
+
+import config from './../config/config'
+
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, 
+                                    useCreateIndex: true,         
+                                    useUnifiedTopology: true } ) 
+
+mongoose.connection.on('error', (err) => {
+  throw new Error(`unable to connect to database ${config.mongoUri}: ${err}`)
+})
 
 app.listen(config.port, (err) => {
   if (err) {
