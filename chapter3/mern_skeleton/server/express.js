50159b44fd85c72f3d143156c37100b1dd280d37
Pick1a1username <xscreensaver@outlook.com>
2022-04-26 22:19:25 +0900

    First Commit for Chapter3 (#4)
    * package initialized. babel, webpack, and others configured.
    
    * packages added and imported.
    
    * server code added. nodemon added.
    
    * nodemon configured. gitignore add
    
    * db configured.
    
    * a template for a root url added
    
    * user model and its helper functions added
    
    * functions for user added.
    
    * functions for user and auth added.
    
    * algorithm for jwt specified. user model moved.

---
+++ chapter3/mern_skeleton/server/express.js
@@ -0,0 +1,37 @@
+import bodyParser from 'body-parser'
+import compress from 'compression'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import express from 'express'
+import helmet from 'helmet'
+
+import Template from './../template'
+import authRoutes from './routes/auth.routes'
+import userRoutes from './routes/user.routes'
+
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.status(200).send(Template())
+})
+app.use('/', userRoutes)
+app.use('/', authRoutes)
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error" : err.name + ": " + err.message})
+  }else if (err) {
+    res.status(400).json({"error" : err.name + ": " + err.message})
+    console.log(err)
+  }
+})
+
+export default app
