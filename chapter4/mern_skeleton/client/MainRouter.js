e15f7a4a881d50d19a091d9f08bf5c3316640148
Pick1a1username <xscreensaver@outlook.com>
2022-04-27 22:25:24 +0900

    Parent component set up.

---
+++ chapter4/mern_skeleton/client/MainRouter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Switch } from 'react-router-dom'
+
+const MainRouter = () => {
+    return ( <div>
+      <Switch>
+      </Switch>
+    </div>
+   )
+}
+
+export default MainRouter
