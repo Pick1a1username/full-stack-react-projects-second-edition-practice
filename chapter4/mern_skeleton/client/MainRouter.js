e4f85b46760845d717bb29863b5122384b33329f
Pick1a1username <xscreensaver@outlook.com>
2022-05-05 22:50:19 +0900

    Signin component added.

--- chapter4/mern_skeleton/client/MainRouter.js
+++ chapter4/mern_skeleton/client/MainRouter.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom' // temporary
 import Home from './core/Home'
 import Users from './user/Users.js'
 import Signup from './user/Signup.js'
+import Signin from './auth/Signin.js'
 
 const MainRouter = () => {
     return ( <div>
@@ -12,10 +13,12 @@ const MainRouter = () => {
         <Route exact path="/" component={Home}/>
         <Route exact path="/users" component={Users}/>
         <Route exact path="/signup" component={Signup}/>
+        <Route path="/signin" component={Signin}/>
       </Switch>
       {/* temporary */}
       <Link to="/users">Users</Link>
       <Link to="/signup">Signup</Link>
+      <Link to="/signin">Signin</Link>
     </div>
    )
 }
