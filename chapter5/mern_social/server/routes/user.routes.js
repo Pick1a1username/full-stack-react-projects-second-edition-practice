8ed14cab15da3c9c862c16703e60942c53fd8e83
Pick1a1username <xscreensaver@outlook.com>
2022-05-24 22:15:15 +0900

    follow feature added.

--- chapter5/mern_social/server/routes/user.routes.js
+++ chapter5/mern_social/server/routes/user.routes.js
@@ -8,16 +8,29 @@ router.route('/api/users')
   .get(userCtrl.list)
   .post(userCtrl.create)
 
+router.route('/api/users/defaultphoto')
+  .get(userCtrl.defaultPhoto)
+
+router.route('/api/users/follow')
+  .put(authCtrl.requireSignin, 
+       userCtrl.addFollowing, 
+       userCtrl.addFollower)
+router.route('/api/users/unfollow')
+  .put(authCtrl.requireSignin, 
+       userCtrl.removeFollowing, 
+       userCtrl.removeFollower)
+
+// routes not including :userId should be located above.
+// If not, userCtrl.userByID() will be called anyway.
+
 router.route('/api/users/:userId')
   .get(authCtrl.requireSignin, userCtrl.read)
   .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
   .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove)
 
-router.param('userId', userCtrl.userByID)
-
 router.route('/api/users/photo/:userId')
   .get(userCtrl.photo, userCtrl.defaultPhoto)
-router.route('/api/users/defaultphoto')
-  .get(userCtrl.defaultPhoto)
-  
+
+router.param('userId', userCtrl.userByID)
+
 export default router
